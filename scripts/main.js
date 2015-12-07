9ff1410c61e297374b646576d3cd5d2e5743c9bc
Richard <rfree86@gmail.com>
2015-12-07 11:23:33 -0500

    added styling, slideTo, working links, and contact section

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+$('#pushMe').click(function(){
+  $.scrollTo($('#work') ,800);
+  return false;
+});
+
+$('#aboutMe').click(function(){
+  $.scrollTo($('#about_me') ,800);
+  return false;
+});
+
+$('#port').click(function(){
+  $.scrollTo($('#work') ,800);
+  return false;
+});
+
+$('#connect').click(function(){
+  $.scrollTo($('#contact') ,800);
+  return false;
+});
